5acf55dd3b16eabdb2c5fbf96ee8feed1f3dfab0
milkymohammed <60290844+milkymohammed@users.noreply.github.com>
2021-12-14 18:54:02 +0300

    Create counting_sort.py

---
+++ day-1/counting_sort.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    x: int = max(arr)
+    if x < 101:
+        x =100
+    count = list(0 for y in range(x))
+    print("count: ", len(count))
+    for z in arr:
+        count[z] += 1
+    return count
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
